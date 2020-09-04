86566a77ff99c11d6b6f12f205cd70bce804d1c3
liemlylac <liemlylac@gmail.com>
2020-09-04 21:08:48 +0700

    ✨ Initial modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,31 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { DirectoryModule } from './directory/directory.module';
+import { GroupModule } from './group/group.module';
+import { HouseModule } from './house/house.module';
+import { BlockModule } from './block/block.module';
+import { FloorModule } from './floor/floor.module';
+import { RoomModule } from './room/room.module';
+import { CustomerModule } from './customer/customer.module';
+import { ContractModule } from './contract/contract.module';
+import { PaymentModule } from './payment/payment.module';
+import { InvoiceModule } from './invoice/invoice.module';
+import { SettingModule } from './setting/setting.module';
 
 @Module({
-  imports: [],
+  imports: [
+    BlockModule,
+    ContractModule,
+    CustomerModule,
+    DirectoryModule,
+    FloorModule,
+    GroupModule,
+    HouseModule,
+    InvoiceModule,
+    PaymentModule,
+    RoomModule,
+    UserModule,
+    SettingModule,
+  ],
 })
 export class AppModule {}
